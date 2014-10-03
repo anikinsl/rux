5339b239df0a80e1b128871dda3b011d9f2ddcb7
anikinsl <anikinsl@gmail.com>
2014-10-03 11:53:03 +0400

    Changed date in the license

--- cpdetect.c
+++ cpdetect.c
@@ -1,5 +1,5 @@
 /*
- * Copyright ¿ 2005-2009 Vyacheslav Anikin. All rights reserved.
+ * Copyright ¿ 2005-2014 Vyacheslav Anikin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
