5e29d069fdb79fe33a58401af70e7b9b10b22737
anikinsl <anikinsl@gmail.com>
2015-02-27 12:30:33 +0300

    Changed version to 0.81

--- version.h
+++ version.h
@@ -1,3 +1,2 @@
-#define VERSION "0.80"
-//#define DATETIME "Wed May 27 00:41:33 MSD 2009"
-#define DATETIME "Wed Oct  1 18:08:50 MSK 2014"
+#define VERSION "0.81"
+#define DATETIME "Fri Feb 27 12:29:12 MSK 2015"
